ac8f54c8699fade0061f5f4f10b7e189d67ebd00
jindongdu <djd28176@hotmail.com>
2023-12-01 15:25:16 -0500

    Initial Commit

---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Jindong Du on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
